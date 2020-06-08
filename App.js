5a550419078ce51074d7f6311989cdf7ce2e9997
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-06-08 13:51:00 +0530

    project setup

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hey world ! I'm Yash Kumar Verma</Text>
+      <br />
+      <Button
+        // onPress={onPressLearnMore}
+        title="Read more about me"
+        color="#841584"
+        accessibilityLabel="Learn more about Yash"
+      />
     </View>
   );
 }
@@ -12,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
