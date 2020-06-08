fc428a9c6ea53c9f4effb4888a959d086881dd1c
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-06-08 14:23:13 +0530

    adding profile picture from github

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  Linking,
+  Alert,
+} from "react-native";
+
+const handlePress = async () => {
+  const url = "https://github.com/yashkumarverma";
+  const supported = await Linking.canOpenURL(url);
+  if (supported) {
+    await Linking.openURL(url);
+  } else {
+    Alert.alert(`Don't know how to open this URL: ${url}`);
+  }
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey world ! I'm Yash Kumar Verma</Text>
-      <br />
+      <Text>{`I'm Yash Kumar Verma \n`}</Text>
+      <Image
+        source={{ uri: "https://avatars0.githubusercontent.com/u/14032427" }}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text>{`\n`}</Text>
       <Button
-        // onPress={onPressLearnMore}
+        onPress={handlePress}
         title="Read more about me"
         color="#841584"
         accessibilityLabel="Learn more about Yash"
